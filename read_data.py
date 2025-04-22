ca28b94370b9da26b692478f09c63731fdade532
jules.shearer <jules.shearer@browncountywi.gov>
2025-04-22 15:09:47 -0500

    Fixed broken links

--- read_data.py
+++ read_data.py
@@ -11,7 +11,7 @@ def read_data(fileLocation):
     data['last_seen'] = pd.to_datetime(data['last_seen'], unit='s').dt.date
     data['ack_dt'] = pd.to_datetime(data['ack_dt'], unit='s').dt.date
     data['closed_dt'] = pd.to_datetime(data['closed_dt'], unit='s').dt.date
-    data['vuln_id.link'] = 'https://app.uncommonx.com/network-disc/vuln/' + data['vuln_id.unique_id'].astype(str)
+    data['vuln_id.link'] = 'https://app.uncommonx.com/network-disc/vuln/' + data['vuln_id.vuln_id'].astype(str)
     return data[['vuln_id.severity', 'vuln_id.name', 'host_id.hostname', 'host_id.ip_address', 'vuln_id.link', 'details.results', 'hvm_id', 'first_seen', 'last_seen', 'vuln_id.exploit']]
 
 if __name__ ==  '__main__':
