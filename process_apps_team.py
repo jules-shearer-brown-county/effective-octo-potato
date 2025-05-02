cb41632298054e93197222077d02511121628551
jules.shearer <jules.shearer@browncountywi.gov>
2025-05-02 09:07:32 -0500

    Added total count for applications team, and commented out the critical only filter

--- process_apps_team.py
+++ process_apps_team.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-#processAppsTeam.py - chopps up a vuln export based on host_id.custom_tags
+#processAppsTeam.py - chops up a vuln export based on host_id.custom_tags
 
 import pandas as pd
 import sys, pyperclip
@@ -12,17 +12,21 @@ else:
 
 def process_apps_team(input_file):
     data = pd.read_excel(input_file)
+    total_count = len(data.index)
     data=data[data['host_id.custom_tags'].notna()]
     #data=data[data['host_id.custom_tags'].str.contains('Apps Team')]
-    data=data[data['vuln_id.severity']>=5]
+    #data=data[data['vuln_id.severity']>=5]
 
     Apps = ['Milestone','CCure', 'LandNAV', 'Papercut', 'v-as400-data', 'OMS', 'Kronos', 'Pinnacle', 'New World', 'Laserfiche', 'AWS', 'County Law']
 
     with pd.ExcelWriter('/mnt/c/Users/Jules.Shearer/Downloads/vulnerability_by_application.xlsx', engine='xlsxwriter') as writer:
+        count = 0
         for app in Apps:
             vulns = data[data['host_id.custom_tags'].str.contains(app)]
+            count += len(vulns.index)
             print("found %d in %s" % (len(vulns.index), app))
             vulns.to_excel(writer, sheet_name=app, index=False)
+        print("found %d in %d records" % (count, total_count))
 
 process_apps_team(input_file)
 
