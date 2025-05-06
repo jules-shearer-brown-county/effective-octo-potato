726eb4c30972f82737fe31e613ea878837f5fd33
jules.shearer <jules.shearer@browncountywi.gov>
2025-05-06 12:02:45 -0500

    added functions for easier time modifying charts/graph

--- proccess_apps_team.py
+++ proccess_apps_team.py
@@ -10,6 +10,7 @@ def open_in_browser(html_path):
     absolute_wsl_path = os.path.join(current_dir, html_path)
     windows_path = subprocess.check_output(["wslpath", "-w", absolute_wsl_path]).decode().strip()
     subprocess.run(["/mnt/c/Program Files/Mozilla Firefox/firefox.exe", windows_path])
+
 def view(fig):
     dir_name = "/mnt/c/Users/Jules.Shearer/Downloads/"
     os.makedirs(dir_name,exist_ok=True)
