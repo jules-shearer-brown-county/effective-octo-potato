ad9f3d69b6941f0383397749f10cb03cfa649135
jules.shearer <jules.shearer@browncountywi.gov>
2025-05-12 10:11:26 -0500

    Added check for ack_dt column in read_data

--- read_data.py
+++ read_data.py
@@ -9,18 +9,12 @@ def read_data(fileLocation):
     data = pd.read_excel(fileLocation)
     data['first_seen'] = pd.to_datetime(data['first_seen'], unit='s').dt.date
     data['last_seen'] = pd.to_datetime(data['last_seen'], unit='s').dt.date
-    #data['ack_dt'] = pd.to_datetime(data['ack_dt'], unit='s').dt.date
     data['closed_dt'] = pd.to_datetime(data['closed_dt'], unit='s').dt.date
     data['vuln_id.link'] = '[link](' + 'https://app.uncommonx.com/network-disc/vuln/' + data['vuln_id.vuln_id'].astype(str) +  ')'
     data['host_id.link'] = '[link](' + 'https://app.uncommonx.com/network-disc/host/' + data['host_id.host_id'].astype(str) +  ')'
+    if( 'ack_dt' in data.columns ):
+        data['ack_dt'] = pd.to_datetime(data['ack_dt'], unit='s').dt.date
     return data.drop(columns=[col for col in data if data[data[col].notna()].empty])
 
 if __name__ ==  '__main__':
-    if len(sys.argv) > 1:
-        # Get information from the command line
-        fileLocation = ''.join(sys.argv[1:])
-    else:
-        #Get location from the clipboard
-        fileLocation = pyperclip.paste()
-
     read_data(fileLocation)
