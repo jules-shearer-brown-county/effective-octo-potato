8d7c795352a99198c0cc7b167a9239919630f4a4
jules.shearer <jules.shearer@browncountywi.gov>
2025-05-12 10:13:48 -0500

    Added get_remediations()

--- remediations_data_table.py
+++ remediations_data_table.py
@@ -1,13 +1,42 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
-import read_data
+import plotly.express as px
+import read_data, utility, proccess_apps_team
 
-df=read_data.read_data('/mnt/c/Users/jules.shearer/Downloads/brown_county_gov_vuln_rememdiation_365.xlsx')
+def severity_pie_chart(grouped_by_severity):
+    grouped_by_severity.rename(
+            index={
+                3:'Medium',
+                4:'High',
+                5:'Critical'
+            },
+            inplace=True),
+    severity_pie = px.pie(
+        grouped_by_severity,
+        names=grouped_by_severity.index,
+        values='hvm_id',
+        title="Number of vulnerabilties severity as % of whole",
+        color=grouped_by_severity.index,
+        color_discrete_map={
+            'Medium':'yellow',
+            'High' : 'orange',
+            'Critical':'red'}
+    )
+    severity_pie.update_traces(textposition='inside', textinfo='percent+label' )
+    return severity_pie
+
+results = utility.get_remediations()
+
+df = read_data.read_data(results)
+
+grouped_by_severity = df[['hvm_id', 'vuln_id.severity']].groupby('vuln_id.severity')
 
 app = Dash()
 
 # Requires Dash 2.17.0 or later
 app.layout = html.Div([
+    html.H4('Remediations'),
+    dcc.Graph(figure=severity_pie_chart(grouped_by_severity.count())),
     dash_table.DataTable(
         df.to_dict('records'),
         [{"name": i, "id": i} for i in df.columns],
@@ -26,4 +55,5 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
+    utility.open_dashboard_in_firefox()
     app.run(debug=True)
