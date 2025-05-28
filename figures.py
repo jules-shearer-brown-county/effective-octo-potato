0810d8ee4e77f50b9327c2ef22b9b8067e904f1d
jules.shearer <jules.shearer@browncountywi.gov>
2025-05-28 10:29:27 -0500

    added methods for showing a generic pie chart, Appllication chart, and for two charts in one figure

--- figures.py
+++ figures.py
@@ -1,10 +1,11 @@
 #!/bin/python
 #figures.py module for holding the shared figures between reports
 from dash import dash_table
+from plotly.subplots import make_subplots
 import utility
 import plotly.express as px
-import pandas as pd
 import plotly.graph_objects as go
+import pandas as pd
 import datetime
 
 def waterfall(df):
@@ -31,7 +32,25 @@ def waterfall(df):
     fig.update_layout(title="Vulnerability Discovery and Resolution")
     return fig
 
+def app_and_severity(df):
+    fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'},{'type':'domain'} ]])
+    fig.add_trace(pie_chart(df, 'vuln_id.severity'), 1,1)
+    fig.add_trace(pie_chart(df, 'Application'), 1,2)
+    fig.update_traces(textposition='inside', textinfo='percent+label+value')
+    fig.update(layout_showlegend=False)
+    return fig
 
+def application_pie_chart(df):
+    grouped_by_application = df.value_counts('Application')
+    application_pie_chart= px.pie(
+        grouped_by_application,
+        names=grouped_by_application.index,
+        values=grouped_by_application.values,
+        title="Number of vulnerabilties, application as % of whole",
+        color=grouped_by_application.index,
+    )
+    application_pie_chart.update_traces(textposition='inside', textinfo='percent+label+value' )
+    return application_pie_chart
 
 def severity_pie_chart(df):
     grouped_by_severity = df.value_counts('vuln_id.severity')
@@ -46,7 +65,7 @@ def severity_pie_chart(df):
         grouped_by_severity,
         names=grouped_by_severity.index,
         values=grouped_by_severity.values,
-        title="Number of vulnerabilties severity as % of whole",
+        title="Number of vulnerabilties, severity as % of whole",
         color=grouped_by_severity.index,
         color_discrete_map={
             'Medium':'yellow',
@@ -56,6 +75,16 @@ def severity_pie_chart(df):
     severity_pie.update_traces(textposition='inside', textinfo='percent+label+value' )
     return severity_pie
 
+def pie_chart(df, col):
+    counts = df.value_counts(col, dropna=False)
+    pie = go.Pie(
+        labels=counts.index,
+        values=counts.values,
+        title= "Percent of whole for %s" % col,
+        name=col
+    )
+    return pie
+
 if __name__ == '__main__':
     df = utility.read_data(utility.get_remediations())
-    utility.view(severity_pie_chart(df))
+    utility.view(pie_chart(df))
